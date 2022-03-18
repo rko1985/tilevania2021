dcb0e04c4cd96bba792fa2589548735859c41a3f
Russell Oyama <russelloyama@gmail.com>
2022-03-18 08:16:29 -1000

    coin pickup sfx

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    [SerializeField] AudioClip coinPickupSFX;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+            Destroy(gameObject);
+        }
+    }
+}
